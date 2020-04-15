d131fded3e722942ba77a8688806c432a08dab30
Seaidear <seaidear323@gmail.com>
2020-04-15 16:24:26 +0200

    Created first iteration of Navbar-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
 
 function App() {
   return (
-    <div>
-      <h1 className="">App</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <h1 className="">App</h1>
+      </div>
+    </>
   );
 }
 
