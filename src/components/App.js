9a96eca8f7751012acce89b9710b01f34c78133c
Seaidear <seaidear323@gmail.com>
2020-04-15 17:03:27 +0200

    Created main view components and implemented basic page routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import AddQuestion from './pages/AddQuestion';
+import Leaderboard from './pages/Leaderboard';
+import { _getQuestions, _getUsers } from '../_DATA';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
+  const [user, setUser] = useState('');
+  const [users, setUsers] = useState(null);
+  const [questions, setquestions] = useState(null);
+
+  useEffect(() => {}, []);
+
   return (
     <>
-      <Navbar />
+      <Navbar loggedInUser={user} />
       <div className="container mt-5">
-        <h1 className="">App</h1>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/add" component={AddQuestion} />
+          <Route exact path="/leaderboard" component={Leaderboard} />
+        </Switch>
       </div>
     </>
   );
