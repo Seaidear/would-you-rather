e1f6d8639fbc697a532146ca7e79bad7129029b1
Seaidear <seaidear323@gmail.com>
2020-04-18 14:12:46 +0200

    Restyled 404-page and added nav-button to Home

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 | Page not found</h1>
+    <div
+      className="card p-3 mx-auto"
+      style={{ width: '65vw', minWidth: '320px', maxWidth: '768px' }}
+    >
+      <h2 className="my-5 text-center">404 | Page not found</h2>
+
+      <div className="text-center mb-5">
+        <Link to="/" className="btn btn-primary">
+          Go to Homepage
+        </Link>
+      </div>
     </div>
   );
 };
