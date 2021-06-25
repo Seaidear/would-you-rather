5d9fb47590b45d562711cf3c98ab27a0a3f99b1e
Seaidear <seaidear323@gmail.com>
2021-06-25 22:14:19 +0200

    minor jsx refactoring

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Footer = () => {
-  return (
-    <div className="text-center mt-5 p-3 bg-primary">
-      <p className="text-white-50 mb-0">Created by Patrick Obermeier</p>
-    </div>
-  );
-};
+const Footer = () => (
+  <div className="text-center mt-5 p-3 bg-primary">
+    <p className="text-white-50 mb-0">Created by Patrick Obermeier</p>
+  </div>
+);
 
 export default Footer;
