f51d61e841a643dfd9d106b68cde3ce668c3be93
Seaidear <seaidear323@gmail.com>
2021-06-25 22:07:48 +0200

    refactored to react-redux hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,37 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Spinner from './layout/shared/Spinner';
 import Login from './user/Login';
 import PageContainer from './layout/shared/PageContainer';
 import MainLayout from './layout/_MainLayout';
 import Routes from './Routes/Routes';
-import { connect } from 'react-redux';
 import { getUsers } from '../actions/users';
 import { getQuestions } from '../actions/questions';
 
-const App = ({ user, loading, getUsers, getQuestions }) => {
+const App = () => {
+  const user = useSelector((state) => state.auth);
+  const loading = useSelector((state) => state.loading);
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    getUsers();
-    getQuestions();
-    // eslint-disable-next-line
-  }, []);
+    dispatch(getUsers());
+    dispatch(getQuestions());
+  }, [dispatch]);
 
   return (
-    <>
-      <MainLayout>
-        {loading ? (
-          <PageContainer>
-            <Spinner />
-          </PageContainer>
-        ) : !user ? (
-          <Login />
-        ) : (
-          <Routes />
-        )}
-      </MainLayout>
-    </>
+    <MainLayout>
+      {loading ? (
+        <PageContainer>
+          <Spinner />
+        </PageContainer>
+      ) : !user ? (
+        <Login />
+      ) : (
+        <Routes />
+      )}
+    </MainLayout>
   );
 };
 
-const mapStateToProps = (state) => ({
-  user: state.auth,
-  loading: state.loading,
-});
-
-export default connect(mapStateToProps, { getUsers, getQuestions })(App);
+export default App;
